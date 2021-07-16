a9f4ffc04bcf8ee53d1abae886ef357a71f2607f
karrybit <takumi.k.5610@gmail.com>
2021-07-17 01:26:12 +0900

    update test

--- test/load.test.ts
+++ test/load.test.ts
@@ -1,11 +1,10 @@
 import { assertEquals } from "https://deno.land/std@0.93.0/testing/asserts.ts"
 import { WasmModule, WasmBuffer } from '../src/wasm.ts';
 
-Deno.test("load module.wat", async() => {
-    const code = await Deno.readFile("data/module.wat");
+Deno.test("load const.wat", async() => {
+    const code = await Deno.readFile("data/const.wasm");
     const wasmBuffer = new WasmBuffer(code);
     const wasmModule = new WasmModule();
     wasmModule.load(wasmBuffer);
-    assertEquals(new Uint8Array([0x00, 0x61, 0x73, 0x6d]), wasmModule.magic)
-    assertEquals(new Uint8Array([0x01, 0x00, 0x00, 0x00]), wasmModule.version)
+    assertEquals(3, wasmModule.sections.length)
 })
