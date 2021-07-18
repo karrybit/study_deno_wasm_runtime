159d30aec254d2e986c3f0bf829f49262aaaf3a4
karrybit <takumi.k.5610@gmail.com>
2021-07-18 12:03:55 +0900

    update test

--- test/store.test.ts
+++ test/store.test.ts
@@ -24,3 +24,11 @@ Deno.test("store const.wasm", async () => {
     wasmModule.store(newBuffer)
     assertEquals(code, newCode)
 })
+
+Deno.test("store const.wasm", async () => {
+    const [wasmModule, wasmBuffer, code] = await loadModule("data/local.wasm")
+    const newCode = new Uint8Array(wasmBuffer.byteLength)
+    const newBuffer = new WasmBuffer(newCode)
+    wasmModule.store(newBuffer)
+    assertEquals(code, newCode)
+})
